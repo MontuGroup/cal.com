fba1d81b72500f0924071216271f2d64c1cdb6fa
Ash D <ashley.d@montu.com.au>
2024-01-31 16:34:06 +1000

    feat: remove doctor and nurse emails from calcom success screen

--- apps/web/modules/bookings/views/bookings-single-view.tsx
+++ apps/web/modules/bookings/views/bookings-single-view.tsx
@@ -450,7 +450,7 @@ export default function Success(props: PageProps) {
                                 </span>
                                 <Badge variant="blue">{t("Host")}</Badge>
                               </div>
-                              <p className="text-default">{bookingInfo.user.email}</p>
+                              {/* Deleted email to hide Doctor/Nurse emails from success screen */}
                             </div>
                           )}
                           {bookingInfo?.attendees.map((attendee) => (
